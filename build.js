a428ab29197e2fb664f3ccb9888444af4385aa24
Anton Khorev <tony29@yandex.ru>
2016-12-11 13:22:39 +0300

    parse segments.osm

---
+++ build.js
@@ -0,0 +1,28 @@
+const fs=require('fs')
+const OSMStream=require('node-osm-stream')
+
+const readWays=(filename,callback)=>{
+	const nodes={}
+	const ways={}
+	const parser=OSMStream()
+	fs.createReadStream(filename).pipe(parser).on('data',()=>{}).on('end',()=>{
+		callback(ways)
+	})
+	parser.on('node',(node,callback)=>{
+		nodes[node.id]=[node.lat,node.lon]
+		callback(null)
+	}).on('way',(way,callback)=>{
+		ways[way.tags.name]={
+			name: way.tags.name,
+			description: way.tags.description,
+			nodes: way.nodes.map(nodeId=>nodes[nodeId]),
+		}
+		callback(null)
+	}).on('relation',(way,callback)=>{
+		callback(null)
+	})
+}
+
+readWays('segments.osm',(ways)=>{
+	console.log(ways['Бойцова пер. 2-8'])
+})
