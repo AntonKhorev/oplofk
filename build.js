df3e07a3e0091a66afa7e664611115fc3a8693a3
Anton Khorev <tony29@yandex.ru>
2017-05-12 07:46:54 +0300

    use oplofk cli

--- build.js
+++
@@ -1,6 +0,0 @@
-const build=require('oplofk-static')
-
-build([
-	['adm','index.html','Обновление POI Адмиралтейского района СПб'],
-	['cen','cen.html','Обновление POI Центрального района СПб'],
-])
